3b4d80e5bc94001c21aab865e70d513c33aca717
Andres Sepulveda <bodyweightforce@gmail.com>
2023-10-26 23:35:37 -0300

    Avance en UI de la plantilla, usando Frame´Â y Line´Â

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,9 +6,9 @@ INSERT INTO salesperson (name) VALUES ('Andres');
 INSERT INTO category (name) VALUES ('Acero');
 
 -- Insert a product
-INSERT INTO products (description, code, weight, price, length, category_id) VALUES (' Perfil C 2x2x0.85 40x38x8x0.85 6000MM', '2000-06000', 4.98, 1500.0, '6000MM', 1);
-INSERT INTO products (description, code, weight, price, length, category_id) VALUES (' Perfil C 2x2x0.85 40x38x8x0.85 2400MM', '2000-02400', 2.04, 1000.0, '2400MM', 1);
-INSERT INTO products (description, code, weight, price, length, category_id) VALUES (' Perfil C 2x2x0.85 40x38x8x0.85 3000MM', '2000-03000', 3.4, 1200.0, '3000MM', 1);
+INSERT INTO products (description, code, weight, price, length, category_id) VALUES ('Perfil C 2x2x0.85 40x38x8x0.85 6000MM', '2000-06000', 4.98, 1500.0, '6000MM', 1);
+INSERT INTO products (description, code, weight, price, length, category_id) VALUES ('Perfil C 2x2x0.85 40x38x8x0.85 2400MM', '2000-02400', 2.04, 1000.0, '2400MM', 1);
+INSERT INTO products (description, code, weight, price, length, category_id) VALUES ('Perfil C 2x2x0.85 40x38x8x0.85 3000MM', '2000-03000', 3.4, 1200.0, '3000MM', 1);
 
 -- Insert a quotation and an item
 INSERT INTO quotations (customer_id, salesperson_id, created_at) VALUES (1, 1, NOW());
